f6acfb47b2dec6add49181ecb33ecccddf74deb9
Blake Griffith <blake.a.griffith@gmail.com>
2024-10-08 14:22:10 -0400

    ra hir attempt

---
+++ src/ra-hir.rs
@@ -0,0 +1,19 @@
+#![allow(unused)]
+use ra_ap_hir::db;
+fn find_function_definitions() {
+    todo!()
+    /*
+    let db = db::RootDatabase::default();
+
+    // Use the database to analyze a crate and get definitions
+    // For example, query for all function definitions in a module
+    let functions = ra_ap_hir::db::find_functions_in_module(db);
+
+    for func in functions {
+        println!("Function name: {}", func.name);
+    }
+        */
+}
+fn main() {
+    find_function_definitions()
+}
