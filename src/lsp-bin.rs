704a698b8c6a10aa18eb79e346c2234b1f75baf7
Blake Griffith <blake.a.griffith@gmail.com>
2024-10-08 13:17:51 -0400

    Is running but rust-analyzer not running so hangs on connect

--- src/lsp-bin.rs
+++ src/lsp-bin.rs
@@ -1,4 +1,4 @@
-use lsp_server::{Connection, Message};
+use lsp_server::{Connection, Message, Request, RequestId};
 use lsp_types::{
     request::GotoDefinition, GotoDefinitionParams, InitializeParams, Position,
     TextDocumentIdentifier, Url,
@@ -10,44 +10,54 @@ fn main() {
     let (connection, io_threads) = Connection::stdio();
 
     // Initialize params for the LSP (to initialize rust-analyzer)
-    let initialize_params = InitializeParams {
+    let initialize_params = serde_json::to_value(InitializeParams {
         process_id: None,
         root_uri: Some(
-            Url::from_file_path(PathBuf::from(".")) // Point to the crate root
+            Url::from_file_path(PathBuf::from("/home/blake/git/impossible")) // Point to the crate root
                 .expect("Invalid path"),
         ),
         ..Default::default()
-    };
+    })
+    .unwrap();
+    dbg!();
 
     // Send the initialize request
-    let initialize_request = connection.initialize(initialize_params).unwrap();
-    if let Message::Response(response) = initialize_request {
-        println!("Received initialize response: {:?}", response);
-    }
+    let initialize_response = connection.initialize(initialize_params).unwrap();
+    println!("Received initialize response: {:?}", initialize_response);
+    dbg!();
 
     // Now you can interact with rust-analyzer via LSP methods, for example:
     let text_document = TextDocumentIdentifier {
-        uri: Url::from_file_path(file!()).unwrap(),
+        uri: Url::from_file_path("src/lsp-bin.rs").unwrap(),
     };
 
+    dbg!();
     let params = GotoDefinitionParams {
         text_document_position_params: lsp_types::TextDocumentPositionParams {
             text_document,
             position: Position::new(3, 5), // Line and column of the function you're analyzing
         },
         work_done_progress_params: Default::default(),
+        partial_result_params: Default::default(),
     };
 
     let request = connection
-        .send_request::<GotoDefinition>(params)
+        .sender
+        .try_send(Message::Request(Request {
+            id: RequestId::from("foo".to_string()),
+            method: "method_string".to_string(),
+            params: serde_json::to_value(params).unwrap(),
+        }))
         .expect("Failed to send definition request");
 
+    dbg!();
     println!("Sent definition request");
 
     // Listen for responses from rust-analyzer
     for msg in connection.receiver.iter() {
         println!("Received message: {:?}", msg);
     }
+    dbg!();
 
     // Wait for IO threads to finish
     io_threads.join().unwrap();
