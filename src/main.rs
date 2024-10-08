0c667a1107b7ebf6d2ac1d9a582d7f998941f9c2
Blake Griffith <blake.a.griffith@gmail.com>
2024-10-08 12:32:20 -0400

    pares given file with tree-sitter

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+//! Parse source using tree-sitter
+//! cargo run src/main.rs
 use std::fs;
 use tree_sitter::{Language, Parser};
 
+fn did_stuff() {
+    println!("yolo");
+}
+// pass in path to source
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let path_to_source = args
+        .get(1)
+        .expect("Must provide path to sourc as second argument");
     // Create a new parser instance
     let mut parser = Parser::new();
 
@@ -13,7 +23,7 @@ fn main() {
 
     // Read a Rust source file (or crate's file)
     let source_code =
-        fs::read_to_string("src/main.rs").expect("Error reading the Rust source file");
+        fs::read_to_string(path_to_source).expect("Error reading the Rust source file");
 
     // Parse the source code into a syntax tree
     let tree = parser
@@ -22,4 +32,5 @@ fn main() {
 
     // Print the root node of the parsed syntax tree
     println!("{:?}", tree.root_node().to_sexp());
+    did_stuff();
 }
